117ec2c2c4447605111cd31d891fbe5bc4a8b946
mmrxistin <mmrxistin@gmail.com>
2026-01-02 05:27:01 +0300

    Subhanallah, Elhamdulillah, Allahu Ekber

--- mela/src/hemanen/web/editor/PostEditor.tsx
+++ mela/src/hemanen/web/editor/PostEditor.tsx
@@ -228,3 +228,41 @@ function AttachmentPreview({
     </div>
   );
 }
+
+function AddAttachmentsButton({
+  onFilesSelected,
+  disabled,
+}: {
+  onFilesSelected: (files: File[]) => void;
+  disabled: boolean;
+}) {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <Button
+        variant="ghost"
+        size="icon"
+        className="text-primary hover:text-primary"
+        disabled={disabled}
+        onClick={() => fileInputRef.current?.click()}
+      >
+        <ImageIcon size={20} />
+      </Button>
+      <input
+        type="file"
+        accept="image/*, video/*"
+        multiple
+        ref={fileInputRef}
+        className="sr-only hidden"
+        onChange={(e) => {
+          const files = Array.from(e.target.files || []);
+          if (files.length) {
+            onFilesSelected(files);
+            e.target.value = "";
+          }
+        }}
+      />
+    </>
+  );
+}
