8cee8b7bc1415ba776f20cb5d6f4df8ab5f10967
mmrxistin <mmrxistin@gmail.com>
2026-01-02 23:30:40 +0300

    Subhanallah, Elhamdulillah, Allahu Ekber

--- mela/src/app/malper/page.tsx
+++ mela/src/app/malper/page.tsx
@@ -28,7 +28,7 @@ function page() {
             />
             <h3 className="text-xl font-bold mb-2">Build Your Website</h3>
             <p className="text-gray-600">Build your website with our easy-to-use templates.</p>
-            <a href="/malper/mmavahi/mmmweb" className="text-blue-500 hover:underline mt-4 block">
+            <a href="/malper/web" className="text-blue-500 hover:underline mt-4 block">
               View Templates and Start Building
             </a>
           </div>
@@ -42,7 +42,7 @@ function page() {
             />
             <h3 className="text-xl font-bold mb-2">Have Your Mobile App </h3>
             <p className="text-gray-600">See how easy it is to build mobile apps with our platform.</p>
-            <a href="/malper/mmavahi/mmmobile" className="text-blue-500 hover:underline mt-4 block">
+            <a href="/malper/mobil" className="text-blue-500 hover:underline mt-4 block">
               Take a Look at Our Mobile App Solutions
             </a>
           </div>
@@ -56,7 +56,7 @@ function page() {
             />
             <h3 className="text-xl font-bold mb-2">Get ai solutions</h3>
             <p className="text-gray-600"> Get AI solutions tailored to your needs.</p>
-            <a href="/malper/mmavahi/mmai" className="text-blue-500 hover:underline mt-4 block">
+            <a href="/malper/jiriyasuni" className="text-blue-500 hover:underline mt-4 block">
               Explore Our AI Solutions
             </a>
           </div>
