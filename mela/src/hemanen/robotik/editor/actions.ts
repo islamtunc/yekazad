f02ecb206fa91871e306a840aaa3bf30ac073483
mmrxistin <mmrxistin@gmail.com>
2026-01-02 04:52:26 +0300

    Subhanallah, Elhamdulillah, Allahu Ekber

--- mela/src/hemanen/robotik/editor/actions.ts
+++ mela/src/hemanen/robotik/editor/actions.ts
@@ -1,11 +1,13 @@
 // Bismillahirrahmanirrahim
 // Elhamdulillahirabbulalemin
-// Es-selatu vesselamu ala resulina Muhammedin ve ala alihi ve sahbihi ecmain
+// Es-selatu vesselamu ala resulina Muhammedin 
+// La ilahe illallah
+// Subhanallah, Elhamdulillah, Allahu Ekber
 "use server";
 
 import { validateRequest } from "@/auth";
 import prisma from "@/pirtukxane/prisma";
-import { getPostDataInclude } from "@/pirtukxane/types";
+import { getRobotikInclude } from "@/pirtukxane/types";
 import { createPostSchema } from "@/pirtukxane/validation";
 
 export async function submitPost(input: {
@@ -18,7 +20,7 @@ export async function submitPost(input: {
 
   const { content, mediaIds } = createPostSchema.parse(input);
 
-  const newPost = await prisma.mmavahi.create({
+  const newPost = await prisma.robotik.create({
     data: {
       content, // Convert string[] to a single string
       userId: user.id,
@@ -26,7 +28,7 @@ export async function submitPost(input: {
         connect: mediaIds.map((id) => ({ id })),
       },
     },
-    include: getPostDataInclude(user.id),
+    include: getRobotikInclude(user.id),
   });
 
   return newPost;
