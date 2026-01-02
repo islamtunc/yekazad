07c9b9e93fd5ad4a5afaa0fb80e1acf5813d9a94
mmrxistin <mmrxistin@gmail.com>
2026-01-02 05:39:17 +0300

    Subhanallah, Elhamdulillah, Allahu Ekber

--- mela/src/hemanen/web/DeletePostDialog.tsx
+++ mela/src/hemanen/web/DeletePostDialog.tsx
@@ -1,7 +1,7 @@
 // Bismillahirrahmanirahim
 // Elhamdulillahi Rabbil Alamin
 // Ve salatu ve selamu ala Resulina Muhammedin 
-// La ilahe illallah, Muhammedur Resulullah
+// La ilahe illallah, Muhammeden abduhu ve resuluhu
 // SuphanAllah velhamdulillah, Allahu Ekber
 // Allah ümmetimizi korusun, birlik ve beraberliğimizi daim eylesin.
 
